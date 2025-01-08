4f9badca2fc4699b985eddbdc37d031f0fad81d3
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2025-01-08 01:52:47 -0800

    Update FizzBuzz.py

--- Leetle/FizzBuzz.py
+++ Leetle/FizzBuzz.py
@@ -34,3 +34,20 @@ def solve(n):
         else:
             result.append(i)
     return result
+
+# DRY
+def solve(n):
+  res = []
+  for i in range(1, n+1):
+      output = ""
+
+      if i % 3 == 0:
+          output += "Fizz"
+      if i % 5 == 0:
+          output += "Buzz"
+
+      if output == "":
+          output = i
+
+      res.append(output)
+  return res
