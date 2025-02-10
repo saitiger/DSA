e876aa1b02d96eb2a0807c1a6f7693e6beb12dc3
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2025-02-09 20:14:36 -0800

    Create Count Prime Numbers.py

---
+++ Leetle/Count Prime Numbers.py
@@ -0,0 +1,10 @@
+def solve(n):
+    if n <= 2:
+        return 0  
+    sieve = [True] * n
+    sieve[0] = sieve[1] = False  
+    for i in range(2, int(n**0.5) + 1):
+        if sieve[i]:  
+            for j in range(i * i, n, i):
+                sieve[j] = False
+    return sum(sieve)
